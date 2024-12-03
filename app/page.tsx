4e9675b82fecbc3551021a16798ca0d3e7f6d399
solalohana <solalohana@gmail.com>
2024-12-03 11:50:29 +0200

    Bento grid To GLOBE DONE

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./components/Hero";
 import { FloatingNav } from "./components/ui/FlaotingNav";
 import { FaHome } from "react-icons/fa";
+import Grid from "./components/Grid";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[{name:'Home',link:'/',icon:<FaHome/>}]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
