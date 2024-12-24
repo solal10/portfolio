af718948d05108b2f58336330984ae962ecf77ef
solalohana <solalohana@gmail.com>
2024-12-24 15:25:25 +0200

    Changed the data

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
         <div className="flex justify-center relative my-20 z-10">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                 <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                    Dynamic Web Magic with Next.js
+                    A portfolio to showcase my skill
                 </h2>
                 <TextGenerateEffect
                 className="text-center text-[40px] md:text-5xk lg:text-6xl"
--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
+      title: "I prioritize stakeholders collaboration, fostering effective communication ",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
@@ -18,7 +18,7 @@ export const navItems = [
     },
     {
       id: 2,
-      title: "I'm very flexible with time zone communications",
+      title: "I am fluent in English, ensuring effective communication",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
@@ -49,7 +49,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
+      title: "Currently learning Nest.js",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -72,68 +72,34 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "https://github.com/adrianhajdin?tab=repositories",
+      title: "Intellitest - AI Model Optimization",
+      des: "Unlock the potential of AI models with IntelliTest's intuitive toolkit for optimizing accuracy.",
+      img: "/p1.png",
+      iconLists: ["/re.svg", "/js.svg", "/python.svg", "/mongo.svg", "/node.svg"],
+      link: "https://github.com/solal10/Intellitest-backend",
     },
     {
       id: 2,
-      title: "Yoom - Video Conferencing App",
-      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/zoom-clone",
-    },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      title: "Bump - Social Media for Gamers",
+      des: "Level up your social experience with Bump. Connect seamlessly with fellow gamers and build your community.",
+      img: "/p2.png",
+      iconLists: ["/js.svg", "/express.svg", "/angular.svg", "/mongo.svg", "/node.svg"],
+      link: "https://github.com/EladSapir/Bump_Backend_Project",
     },
   ];
   
   export const testimonials = [
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "Solal worked as a teaching assistant in SW Project Managemnt, where he instructed and led several teams in submitting a sw project . Elad demonstrated leadership, responsiability, as well as high level serviceability, detailed oriented, and managed crisses in a calm and efficient manner. In addition, I have supervised the final project of Elad, where he demonstrated high passion for technology, outstanding learning curve, planning skills, and pursuing excellence, which were reflected in high-level project.",
+      name: "Benny Sand",
+      title: "VP Knowledge and Innovation of Qualitest",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+        "One of our best students: sharp, curious, and hardworking! I am confident that those who work with Solal will benefit from his exceptional talent and strong interpersonal skills.",
+      name: "Hadassa Daltrophe",
+      title: "Software Engineering Lecturer at SCE",
     },
   ];
   
@@ -173,29 +139,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Teaching Assistant",
+      desc: "Guided project preparation and reviews for 80 students in Software Engineering courses, including Requirements, Management, Testing, and Quality Assurance.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Backend Developer",
+      desc: "Developed scalable microservices and APIs, integrating data for frontend use while ensuring clean, reusable Python code. Designed and maintained server-side applications with MongoDB, SQL, and PostgreSQL, delivering features efficiently in collaboration with the team.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Research Assistant",
+      desc: "Developed and optimized SVC classification models in PyTorch, analyzing predictions to support research reporting.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Bachelor Degree in Software Engineering ",
+      desc: "Sami Shamoon College of Engineering (SCE), GPA: 90, with coursework in Data Structures, Algorithms, OOP, Networking, and Databases.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
